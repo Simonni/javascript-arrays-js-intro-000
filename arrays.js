29bf6d1936926536771fb65c2c4ca7eadbae58f6
Simon Kiflay <szkiflay@gmail.com>
2018-08-13 12:08:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand'",  "kitkat", "skittles"];
 function addElementToBeginningOfArray (array, element) {
-  chocolateBars.unshift"array,", "element");
+  chocolateBars.unshift("array,", "element");
+  return addElementToBeginningOfArray;
   }
+   function destructively
   
   
-  
-/*= chocolateBars.unshift("milky way");
-var destructivelyAddElementToBeginningOfArray = ("3 musketeers",...chocolateBars);
-var addElementToBeginningOfArray = ("almond joy",...chocolateBars);
-var destructivelyAddElementToBeginningOfArray= (chocolateBars,..."milky way");  
-var accessElementInArray = ["cnn", "bbc", "nbc"];
-console.log([2]);
-var destructivelyRemoveElementFromBeginningOfArray = ["cnn", "bbc", "nbc", "fox news"];
-destructivelyRemoveElementFromBeginningOfArray.slice(3);
-var remove = removeElementToBeginningOfArray.slice(-2);
-var popLast = destructivelyRemoveElementFromBeginningOfArray.pop*/();
