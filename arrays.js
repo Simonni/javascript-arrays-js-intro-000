ae0819c5fcfdbb5cbcf3d3a5230e9c2fb19f5a88
Simon Kiflay <szkiflay@gmail.com>
2018-08-13 11:05:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,4 @@ console.log([2]);
 var destructivelyRemoveElementFromBeginningOfArray = ['cnn', 'bbc', 'nbc', 'fox news'];
 destructivelyRemoveElementFromBeginningOfArray.slice(3);
 var remove = removeElementToBeginningOfArray.slice(-2);
+var popLast = destructivelyRemoveElementFromBeginningOfArray
