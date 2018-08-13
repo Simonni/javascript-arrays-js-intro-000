ff9a081339350f89c15c58830f9c5de44871dd21
Simon Kiflay <szkiflay@gmail.com>
2018-08-13 11:51:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand'",  "kitkat", "skittles"];
-var addElementToBeginningOfArray = chocolateBars.unshift('milky way');
-var destructivelyAddElementToBeginningOfArray = ('3 musketeers',...chocolateBars);
-var addElementToBeginningOfArray = ('almond joy',...chocolateBars);
-var destructivelyAddElementToBeginningOfArray= (chocolateBars,...'milky way');  
-var accessElementInArray = ['cnn', 'bbc', 'nbc'];
+var addElementToBeginningOfArray = chocolateBars.unshift("milky way");
+var destructivelyAddElementToBeginningOfArray = ("3 musketeers",...chocolateBars);
+var addElementToBeginningOfArray = ("almond joy",...chocolateBars);
+var destructivelyAddElementToBeginningOfArray= (chocolateBars,..."milky way");  
+var accessElementInArray = ["cnn", "bbc", "nbc"];
 console.log([2]);
-var destructivelyRemoveElementFromBeginningOfArray = ['cnn', 'bbc', 'nbc', 'fox news'];
+var destructivelyRemoveElementFromBeginningOfArray = ["cnn", "bbc", "nbc", "fox news"];
 destructivelyRemoveElementFromBeginningOfArray.slice(3);
 var remove = removeElementToBeginningOfArray.slice(-2);
 var popLast = destructivelyRemoveElementFromBeginningOfArray.pop();
