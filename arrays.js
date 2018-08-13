d674061798d303b8bd0c0c6fbccc0304b1fb6e28
Simon Kiflay <szkiflay@gmail.com>
2018-08-13 10:37:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,5 @@ var addElementToBeginningOfArray = chocolateBars.unshift('milky way');
 var destructivelyAddElementToBeginningOfArray = ('3 musketeers',...chocolateBars);
 var addElementToBeginningOfArray = ('almond joy',...chocolateBars);
 var destructivelyAddElementToBeginningOfArray= (chocolateBars,...'milky way');  
+var accessElementInArray = ['cnn', 'bbc', 'nbc'];
+console.log([2]);
