adb59100c16c01e955e070f7dc90440c229cfc8b
Simon Kiflay <szkiflay@gmail.com>
2018-08-13 12:01:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand'",  "kitkat", "skittles"];
-function addElementToBeginningOfArray = chocolateBars.unshift("milky way");
+function addElementToBeginningOfArray () {
+  chocolateBars.unshift"array,", "element");
+  }
+  
+  
+  
+/*= chocolateBars.unshift("milky way");
 var destructivelyAddElementToBeginningOfArray = ("3 musketeers",...chocolateBars);
 var addElementToBeginningOfArray = ("almond joy",...chocolateBars);
 var destructivelyAddElementToBeginningOfArray= (chocolateBars,..."milky way");  
@@ -8,4 +14,4 @@ console.log([2]);
 var destructivelyRemoveElementFromBeginningOfArray = ["cnn", "bbc", "nbc", "fox news"];
 destructivelyRemoveElementFromBeginningOfArray.slice(3);
 var remove = removeElementToBeginningOfArray.slice(-2);
-var popLast = destructivelyRemoveElementFromBeginningOfArray.pop();
+var popLast = destructivelyRemoveElementFromBeginningOfArray.pop*/();
